c818c431dd7088a285d80cc834a1b2d454ebc346
Sergei Lisitsyn <autoanastasia@narod.ru>
2021-07-10 11:26:53 +0300

    main.go works from terminal

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -1,8 +1,13 @@
 package fibonacci
 
+import (
+	"fmt"
+)
+
 var F1 int32
 var F2 int32
 
+// funkFibo prints k first Fibonacci numbers
 func Fibo(k int) (Fibo []int32) {
 	F1, F2 = 0, 1
 	Fibo = make([]int32, 1)
@@ -16,5 +21,19 @@ func Fibo(k int) (Fibo []int32) {
 		return
 	} else {
 		return
+	}
+} //Print n memeber of Fibonacci sequence
+func ShowFibo(n int) {
+	var fiboSlice []int32
+	fiboSlice = Fibo(n)
+	fmt.Print("The ", n, "-th Fibonacci number = ", fiboSlice[n-1], ".")
+
+} // Print Fibonacci sequence in column
+func FiboInColumn(n int) {
+	var fiboSlice []int32
+	fiboSlice = Fibo(n)
+	fmt.Printf("\n%s %s", "Numer", "Fibo number")
+	for k, f := range fiboSlice {
+		fmt.Printf("\n%5d %5d", k, f)
 	}
 }
--- lesson02/main.go
+++ lesson02/main.go
@@ -7,9 +7,18 @@ import (
 )
 
 func main() {
-	var f int = 12 // Quontity of Fibonacci numbers
+	defer fmt.Println("\nThe Progam has successfully completed!") //Complete massage
+	fmt.Println("Hello, please input f - qauntity of Fibonacci numbers you want to print")
+	var f int
+	fmt.Scanf("%d", &f)
+	fmt.Println("You entered:", f)
+	//	f = 12 // Size of Fibonacci sequence
+	defer fmt.Print("\nNow you see sequence of ", f, " Fibonacci numbers.")
 	var fiboArr []int32
 	fiboArr = fibonacci.Fibo(f)
 	fmt.Println(fiboArr)
+	fmt.Print()
+	fibonacci.ShowFibo(10)
+	fibonacci.FiboInColumn(f)
 
 }

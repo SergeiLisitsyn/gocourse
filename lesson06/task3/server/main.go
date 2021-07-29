843a7dc8e5612b8ba32f9802fc44e7c6f63cfe51
Sergei Lisitsyn <autoanastasia@narod.ru>
2021-07-29 21:12:57 +0300

    Cookie fixed

---
+++ lesson06/task3/server/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+var name string
+var address string
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+
+		// GET return HTML page
+		http.ServeFile(w, r, "form.html")
+
+	case "POST":
+
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+
+		name = r.FormValue("name")
+
+		address = r.FormValue("address")
+
+		fmt.Fprintf(w, "%s from %s\n", name, address)
+		fmt.Printf("%s from %s\n", name, address)
+	/*	expire := time.Now().Add(200 * time.Minute)
+		cookie := &http.Cookie{
+			Name:    "token#1",
+			Value:   fmt.Sprintf("%s:%s", name, address),
+			MaxAge:  300,
+			Expires: expire,
+		}
+
+		http.SetCookie(w, cookie)
+		w.WriteHeader(200)
+		fmt.Println(name)
+		w.Write([]byte(fmt.Sprintf("%s Get Successful\n", cookie.Name)))
+
+		return*/
+
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
+}
+
+func docHandler(w http.ResponseWriter, r *http.Request) {
+
+	expire := time.Now().Add(200 * time.Minute)
+	cookie := &http.Cookie{
+		Name:    "token#1",
+		Value:   fmt.Sprintf("%s:%s", name, address),
+		MaxAge:  300,
+		Expires: expire,
+	}
+
+	http.SetCookie(w, cookie)
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("%s Get Successful\n", cookie.Name)))
+
+	return
+}
+
+func main() {
+	var port int = 8080
+	fmt.Printf("Server started at port %d\n", port)
+	http.HandleFunc("/", handler)
+	docHandler := http.HandlerFunc(docHandler)
+	http.Handle("/doc", docHandler)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%d", port), nil))
+}

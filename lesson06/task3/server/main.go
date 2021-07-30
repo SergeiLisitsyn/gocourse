5157da01bafe312fb8eefd41b8a2895db66e1e8e
Sergei Lisitsyn <autoanastasia@narod.ru>
2021-07-30 22:54:57 +0300

    Server Cookie fixed

--- lesson06/task3/server/main.go
+++ lesson06/task3/server/main.go
@@ -29,46 +29,35 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
-
 		name = r.FormValue("name")
-
 		address = r.FormValue("address")
 
-		fmt.Fprintf(w, "%s from %s\n", name, address)
-		fmt.Printf("%s from %s\n", name, address)
+		expire := time.Now().Add(200 * time.Minute)
+		cookie := &http.Cookie{
+			Name:    "token#1",
+			Value:   fmt.Sprintf("%s:%s", name, address),
+			MaxAge:  300,
+			Expires: expire,
+		}
+
+		http.SetCookie(w, cookie)
+
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("Cookie name: %s is setted successful\n", cookie.Name)))
+
+		//Print Cookie
+		fmt.Fprintf(w, "%s\n", cookie)
+		fmt.Println(cookie)
 
 	default:
-		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+		fmt.Fprintf(w, "404 not found.")
 	}
-}
-
-func docHandler(w http.ResponseWriter, r *http.Request) {
-
-	expire := time.Now().Add(200 * time.Minute)
-	cookie := &http.Cookie{
-		Name:    "token#1",
-		Value:   fmt.Sprintf("%s:%s", name, address),
-		MaxAge:  300,
-		Expires: expire,
-	}
-
-	http.SetCookie(w, cookie)
-
-	w.WriteHeader(200)
-	w.Write([]byte(fmt.Sprintf("%s Get Successful\n", cookie.Name)))
-
-	//Print Cookie
-
-	fmt.Println(cookie)
-
-	return
 }
 
 func main() {
 	var port int = 8080
 	fmt.Printf("Server started at port %d\n", port)
 	http.HandleFunc("/", handler)
-	docHandler := http.HandlerFunc(docHandler)
-	http.Handle("/doc", docHandler)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

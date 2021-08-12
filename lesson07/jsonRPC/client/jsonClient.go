66b5bb9130c17ee237721b453c1b7065305815be
Sergei Lisitsyn <autoanastasia@narod.ru>
2021-08-12 14:28:26 +0300

    jsonClient is refactored

--- lesson07/jsonRPC/client/jsonClient.go
+++ lesson07/jsonRPC/client/jsonClient.go
@@ -20,31 +20,47 @@ func main() {
 	}
 	in := bufio.NewReader(os.Stdin) //Create reader for input data
 	//Loop to input three names with one of them - John
-	for i := 0; i < 3; i++ {
-		fmt.Println("Input Name to add to the List")
-		name, _, err := in.ReadLine()
+	for {
+		fmt.Println("If you want to add name in register type <1>.\nIf you want to resive full list of the items, type <2>. ")
+
+		choose, _, err := in.ReadLine()
 		if err != nil {
 			log.Fatal(err)
 		}
-		//Call the procedure to input name to database
-		err = client.Call("API.Register", name, &reply)
-		if err != nil {
-			log.Fatal(err)
+
+		switch procedure := string(choose); procedure {
+
+		case "1":
+			{
+				fmt.Println("Please type name you want to add in list.")
+				name, _, err := in.ReadLine()
+				if err != nil {
+					log.Fatal(err)
+				}
+				//Call the procedure to input name to database
+				err = client.Call("API.Register", name, &reply)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("New name: %v witht ID %v added to the list", reply.Name, reply.ID)
+			}
+		case "2":
+			{
+				//Call the procedure to get database
+				err = client.Call("API.GetList", "GetList", &list)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println("List :")
+				var count = 1
+				for _, v := range list.Database {
+					fmt.Printf("%v) ID: %v Name: %v;\n", count, v.ID, v.Name)
+					count++
+				}
+			}
+		default:
+			fmt.Println("You did not choose. Action 3, which doesn't exist")
+
 		}
-		log.Printf("New name: %v witht ID %v added to the list", reply.Name, reply.ID)
 	}
-	//Call the procedure to get database
-	err = client.Call("API.GetList", "GetList", &list)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("List %v:", list)
-
-	//Try to input the name John second time.
-	err = client.Call("API.Register", []byte("John"), &reply)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("New name: %v witht ID %v added to the list", reply.Name, reply.ID)
-
 }

41e7e2ee2799769a1d50c70c810f1ac9e102e395
Sergei Lisitsyn <autoanastasia@narod.ru>
2021-08-19 15:04:29 +0300

    path for method search changed

--- lesson07/rest/course-rest-master/cmd/rest-pre/main.go
+++ lesson07/rest/course-rest-master/cmd/rest-pre/main.go
@@ -17,7 +17,7 @@ func main() {
 	router.HandleFunc("/api/v0/user/{id}", user.Delete).Methods(http.MethodDelete)
 	router.HandleFunc("/api/v0/user/{id}", user.View).Methods(http.MethodGet)
 	router.HandleFunc("/api/v0/user/{id}", user.Update).Methods(http.MethodPut)
-	router.HandleFunc("/api/v0/user/", user.Search).Methods(http.MethodPost)
+	router.HandleFunc("/api/v0/search/", user.Search).Methods(http.MethodPost)
 	log.Println("Populating data")
 	p := stub_contacts.LoadPersistent()
 	if err := stub_contacts.Populate(p); err != nil {
